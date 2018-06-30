63b010921a0c15c9b2e096770306aafec0e7273b
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-06-30 14:55:30 +0300

    Visualize sample data with a better chart

--- App.js
+++ App.js
@@ -1,26 +1,82 @@
 import React, { Component } from 'react';
-import { XYPlot, LineSeries } from 'react-vis';
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  VerticalGridLines,
+  HorizontalGridLines,
+  LineSeries,
+  Crosshair
+} from 'react-vis';
 import './node_modules/react-vis/dist/style.css';
 
+const DATA = [
+  [
+    {x: 0, y: 158},
+    {x: 1, y: 143},
+    {x: 2, y: 148},
+    {x: 3, y: 136},
+    {x: 4, y: 143},
+    {x: 5, y: 144},
+    {x: 6, y: 141},
+    {x: 7, y: 140},
+    {x: 8, y: 137},
+    {x: 9, y: 146}
+  ],
+  [
+    {x: 0, y: 93},
+    {x: 1, y: 95},
+    {x: 2, y: 99},
+    {x: 3, y: 92},
+    {x: 4, y: 91},
+    {x: 5, y: 92},
+    {x: 6, y: 96},
+    {x: 7, y: 93},
+    {x: 8, y: 87},
+    {x: 9, y: 90}
+  ]
+];
+
+const PLOT_WIDTH = 900;
+
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = { crosshairValues: [] };
+
+    this._onNearestX = this._onNearestX.bind(this);
+    this._onMouseLeave = this._onMouseLeave.bind(this);
+  }
+
+  _onNearestX(value, { index }) {
+    this.setState({ crosshairValues: DATA.map(d => d[index]) });
+  }
+
+  _onMouseLeave() {
+    this.setState({ crosshairValues: [] });
+  }
+
   render() {
-    const data = [
-      {x: 0, y: 8},
-      {x: 1, y: 5},
-      {x: 2, y: 4},
-      {x: 3, y: 9},
-      {x: 4, y: 1},
-      {x: 5, y: 7},
-      {x: 6, y: 6},
-      {x: 7, y: 3},
-      {x: 8, y: 2},
-      {x: 9, y: 0}
-    ];
 
     return (
-      <div className='app'>
-        <XYPlot height={300} width={300}>
-          <LineSeries data={data} />
+      <div className='app' style={{
+        'margin-left': 'auto',
+        'margin-right': 'auto',
+        'width': PLOT_WIDTH
+      }}>
+        <XYPlot
+          onMouseLeave={this._onMouseLeave}
+          height={600}
+          width={PLOT_WIDTH}>
+          <VerticalGridLines/>
+          <HorizontalGridLines/>
+          <XAxis/>
+          <YAxis/>
+          <LineSeries
+            onNearestX={this._onNearestX}
+            data={DATA[0]} />
+          <LineSeries data={DATA[1]} />
+          <Crosshair values={this.state.crosshairValues} className={'test-class-name'} />
         </XYPlot>
       </div>
     );
