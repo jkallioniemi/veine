ef084fe12940920ffac14d3ca2e95a5dec0d5235
Juuso Kallioniemi <juuso.kallioniemi@vincit.fi>
2018-07-08 18:26:53 +0300

    Average measurements
    Previous behaviour discarded measuremenets that did not have a
    timestamp, this implementation averages all measurements instead.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Chart from './components/chart';
 import myData from '../bpdata.csv';
-import { __, pick, map, curry, reduce, assoc, keys, addIndex, filter } from 'ramda';
+import { __, pick, map, curry, reduce, assoc, keys, addIndex, filter, splitEvery, mean } from 'ramda';
 import parse from 'csv-parse/lib/sync';
+
+const TIME_COLUMN_NAME = 'Time';
 
 let csvData = parse(myData, { columns: true });
 
@@ -10,8 +12,17 @@ const renameKeys = curry((keysMap, obj) =>
   reduce((acc, key) => assoc(keysMap[key] || key, obj[key], acc), {}, keys(obj))
 );
 
+const averageTwoElements = curry((column, item) =>
+  ({
+    [TIME_COLUMN_NAME]: item[0][TIME_COLUMN_NAME],
+    [column]: mean([item[0][column], item[1][column]])
+  })
+);
+
 const getTimeAndColumn = (column) => {
   let data = map(pick(['Time', column]), csvData);
+  data = splitEvery(2, data);
+  data = map(averageTwoElements(column), data);
   const renames = { Time: 'x' };
   renames[column] = 'y';
   data = map(renameKeys(renames), data);
