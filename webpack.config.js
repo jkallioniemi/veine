55d4820b6c4b21c731d150852eb7ca961c209786
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-07-11 21:11:51 +0300

    Source maps & optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const config = {
   entry: [
     './index.js',
   ],
+  devtool: 'eval-source-map',
   module: {
     rules: [
       {
@@ -26,6 +27,27 @@ const config = {
       }
     ],
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      name: true,
+      cacheGroups: {
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        }
+      }
+    }
+  }
 };
 
 module.exports = config;
